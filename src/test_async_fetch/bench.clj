fc6ea458001ea7c6296e0740d6938615b780c4a0
Nils Grunwald <nils@grunwald.fr>
2011-03-24 02:19:53 +0100

    added async

--- src/test_async_fetch/bench.clj
+++ src/test_async_fetch/bench.clj
@@ -1,18 +1,41 @@
 (ns test-async-fetch.bench
   (:use lamina.core
-        [clojure.contrib.io :only [read-lines]])
-  (:require [clj-http.client :as c]))
+        [clojure.contrib.io :only [read-lines]]
+        clojure.contrib.profile)
+  (:require [clj-http.client :as c]
+            [http.async.client :as a]))
 
-(def urls (read-lines "urls.txt"))
+(defn get-urls []  (read-lines "urls.txt"))
+
+(defn fetch-with-apache
+  [url]
+  (->
+   (c/get url)
+   (:status)))
 
 (defn test-lamina
   [urls]
-  (time
-   (let [chan (apply channel urls)]
-     (receive-all chan (fn [url]
-                         (->
-                          (c/get url)
-                          (:status)))))))
+  (prof :lamina
+        (let [chan (apply channel urls)]
+          (receive-all chan fetch-with-apache))))
 
+(defn test-sync
+  [urls]
+  (prof :sync
+        (for [url urls]
+          (fetch-with-apache url))))
 
+(defn test-pmap
+  [urls]
+  (prof :pmap
+        (pmap fetch-with-apache urls)))
 
+(defn test-async
+  [urls]
+  (prof :async
+        (a/with-client {:follow-redirects true}
+          (let [arr (map #(a/GET %) urls)]
+            (for [res arr]
+              (do
+                (a/await res)
+                (:code (a/status res))))))))
