dc2fad3099bc4ca6f308f0a01721633557238514
Nils Grunwald <nils@grunwald.fr>
2011-03-24 03:06:49 +0100

    strange results

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
                  [org.clojure/clojure-contrib "1.2.0"]
                  [lamina "0.4.0-SNAPSHOT"]
                  [http.async.client "0.2.2"]
-                 [clj-http "0.1.3"]])
+                 [clj-http "0.1.3"]]
+  :main test-async-fetch.bench)
--- src/test_async_fetch/bench.clj
+++ src/test_async_fetch/bench.clj
@@ -1,41 +1,43 @@
 (ns test-async-fetch.bench
   (:use lamina.core
-        [clojure.contrib.io :only [read-lines]]
-        clojure.contrib.profile)
+        [clojure.contrib.io :only [read-lines]])
   (:require [clj-http.client :as c]
             [http.async.client :as a]))
 
-(defn get-urls []  (read-lines "urls.txt"))
+(defn get-urls [file] (read-lines file))
 
 (defn fetch-with-apache
   [url]
-  (->
-   (c/get url)
-   (:status)))
+  (try
+    (->
+     (c/get url)
+     (:status))
+    (catch Exception e
+      400)))
 
 (defn test-lamina
   [urls]
-  (prof :lamina
-        (let [chan (apply channel urls)]
-          (receive-all chan fetch-with-apache))))
+   (let [chan (apply channel urls)]
+     (receive-all chan fetch-with-apache)))
 
 (defn test-sync
   [urls]
-  (prof :sync
-        (for [url urls]
-          (fetch-with-apache url))))
+   (for [url urls]
+     (fetch-with-apache url)))
 
 (defn test-pmap
   [urls]
-  (prof :pmap
-        (pmap fetch-with-apache urls)))
+   (pmap fetch-with-apache urls))
 
 (defn test-async
   [urls]
-  (prof :async
-        (a/with-client {:follow-redirects true}
-          (let [arr (map #(a/GET %) urls)]
-            (for [res arr]
-              (do
-                (a/await res)
-                (:code (a/status res))))))))
+   (a/with-client {:follow-redirects true}
+     (let [arr (map #(a/GET %) urls)]
+       (for [res arr]
+         (do
+           (a/await res)
+           (:code (a/status res)))))))
+
+(defn -main
+  [file]
+  (test-pmap (get-urls file)))
